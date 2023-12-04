b301d1250f314b31bb9a6593d0ff134fca6d09b3
Lionel Castell <lionel.castell@sciacademy.org>
2023-12-04 16:24:18 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -26,17 +26,13 @@ var init = function (window) {
 
         // TODO 2 : Create a function that draws a circle 
 
-        var drawCircle = function ()}
-//create the circle
-   //randomCircleInArea(area, 
-circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-   
-    // set motion proporties 
-    physikz.addRandomVelocity(circle, canvas, 11, 11);
-    
-    //add the circle to circle array, add to  view
-    view.addChild(circle);
-    circles.push(circle);
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
+    }
 
     // TODO 3 / 7 : Call the drawCircle() function 
 
@@ -46,7 +42,7 @@ circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
     drawCircle();
     drawCircle();
 
-    
+
 
 
 
@@ -66,12 +62,12 @@ circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
         physikz.updatePosition(circles[2]);
         physikz.updatePosition(circles[3]);
         physikz.updatePosition(circles[4]);
-      
+
 
 
         // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-        game.checkCirclePosition(circles[0]);
-        game.checkCirclePosition(circles[1]);
+        game.checkCirclePosition(circless[0]);
+        game.checkCirclePosition(circle[1]);
         game.checkCirclePosition(circles[2]);
         game.checkCirclePosition(circles[3]);
         game.checkCirclePosition(circles[4]);
@@ -80,41 +76,40 @@ circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
         // TODO 9 : Iterate over the array
 
 
-    }
 
-    /* 
-    This Function should check the position of a circle that is passed to the 
-    Function. If that circle drifts off the screen, this Function should move
-    it to the opposite side of the screen.
-    */
-    game.checkCirclePosition = function (circle) {
+        /* 
+        This Function should check the position of a circle that is passed to the 
+        Function. If that circle drifts off the screen, this Function should move
+        it to the opposite side of the screen.
+        */
+        game.checkCirclePosition = function (circle) {
 
-        // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-        if (circle.x > canvas.width) {
-            circle.x = 0;
+            // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
+            if (circle.x > canvas.width) {
+                circle.x = 0;
+            }
+
+            // TODO 6 : YOUR CODE STARTS HERE //////////////////////
+
+
+
+            // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
 
-        // TODO 6 : YOUR CODE STARTS HERE //////////////////////
+        /////////////////////////////////////////////////////////////
+        // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
+        /////////////////////////////////////////////////////////////
 
+        view.addChild(fps);
+        app.addUpdateable(fps);
 
+        game.circle = circle;
+        game.circles = circles;
+        game.drawCircle = drawCircle;
+        game.update = update;
 
-        // YOUR TODO 6 CODE ENDS HERE //////////////////////////
+        app.addUpdateable(window.opspark.game);
     }
-
-    /////////////////////////////////////////////////////////////
-    // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
-    /////////////////////////////////////////////////////////////
-
-    view.addChild(fps);
-    app.addUpdateable(fps);
-
-    game.circle = circle;
-    game.circles = circles;
-    game.drawCircle = drawCircle;
-    game.update = update;
-
-    app.addUpdateable(window.opspark.game);
-}
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
